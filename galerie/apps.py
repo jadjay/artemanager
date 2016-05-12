d6491cbe4cf2997a0afb77d6dc8f796d3444152a
Jérôme Avond <jerome@rachetjay.fr>
2016-05-12 21:17:45 +0200

    Initial

---
+++ galerie/apps.py
@@ -0,0 +1,9 @@
+from django.utils.translation import gettext_lazy as _
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class GalerieConfig(AppConfig):
+    name = 'galerie'
