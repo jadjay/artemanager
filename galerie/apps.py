27fb093f76a32ca700c3b31e5a745961495bf9cd
Jérôme Avond <jerome@rachetjay.fr>
2016-05-13 01:46:08 +0200

    Revert "Initial"
    This reverts commit d6491cbe4cf2997a0afb77d6dc8f796d3444152a.
    
    Conflicts:
    	.gitignore
    	galerie/models.py

--- galerie/apps.py
+++
@@ -1,9 +0,0 @@
-from django.utils.translation import gettext_lazy as _
-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class GalerieConfig(AppConfig):
-    name = 'galerie'
